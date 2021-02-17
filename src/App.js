0135ee298f8697286b5629b28db1dfba3d46704d
Carmenprz <carmenp.gomez99@gmail.com>
2021-02-17 21:38:07 +0100

    MessageList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react'; 
-import NewMessageForm from './components/NewMessageForm'
+import React, { useState } from 'react'; 
+import NewMessageForm from './components/NewMessageForm';
+import MessageList from './components/MessageList';
 
 const App = () => {
-  const handleSend = newMessage => {};
+  const [messages, setMessages] = useState([]);
+  const handleSend = newMessage => {
+    setMessages([newMessage, ...messages]);
+  };
 
   return (
     <div>
       <NewMessageForm onSend={handleSend}/>
+      <MessageList data={messages} />
     </div>
   )
 } 
